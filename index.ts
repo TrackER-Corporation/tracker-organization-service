e222699b74056a04e35b169b267fae85b7f57c22
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-25 17:02:53 +0200

    feat: update api route

--- index.ts
+++ index.ts
@@ -2,22 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { connectToDatabase } from './db/services/database.service';
+import route from "./db/route/route"
 
 dotenv.config();
-
 const port = process.env.PORT;
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(
-  '/',
-  (req, res, next) => {
-    return res.status(200).json({ "msg": "test" })
-  });
-
 connectToDatabase()
+
+app.use(route);
+
 app.listen(port, () => console.info(`tracker-organization-service is running`));
 
 
