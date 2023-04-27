e8b013c52befb2d2e629df431d84af5b1eacb58f
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-04-27 17:10:26 +0200

    test: connection to db

---
+++ test/database.service.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, it } from "vitest";
+import * as mongoDB from 'mongodb';
+import { collections, connectToDatabase } from "../db/services/database.service";
+
+
+describe('connectToDatabase', () => {
+    it('should connect to the database and set up the organizations collection', async () => {
+
+        // Connect to the database
+        await connectToDatabase();
+
+        // Check that the organizations collection has been set up
+        expect(collections.organizations).toBeInstanceOf(mongoDB.Collection);
+
+        // Check that the connection was successful by inserting a new building
+        const building = {
+            name: 'Test Building',
+            contact: 'test@example.com',
+            address: '123 Main St, Anytown USA',
+            type: 'Office',
+            organizationId: '610a96a9f9d9b935a42a50a2',
+            sqft: 1000,
+            lat: 40.7128,
+            long: -74.0060,
+            resources: []
+        };
+
+        const result = await collections.organizations?.insertOne(building);
+        expect(result?.acknowledged).toBe(true);
+
+        // Clean up by deleting the test building
+        await collections.organizations?.deleteOne({ _id: result?.insertedId });
+    });
+});
