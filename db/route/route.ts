12558d8a6177a6cde86a576a84ea53d971046c2e
Piccio98 <104207695+Piccio98@users.noreply.github.com>
2023-04-29 16:30:54 +0200

    feat: update route

--- db/route/route.ts
+++ db/route/route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import { createOrganization, updateOrganization, getOrganizationById, getOrganizationByUserId, deleteOrganization, getAll, updateOrganizationResources } from '../controller/controller';
+import { createOrganization, updateOrganization, getOrganizationById, getOrganizationByUserId, deleteOrganization, getAll, updateOrganizationResources, deleteOrganizationByUserId } from '../controller/controller';
 
 router.get('/all', getAll);
 router.get('/user/:id', getOrganizationByUserId);
@@ -9,5 +9,6 @@ router.post('/', createOrganization);
 router.put('/:id', updateOrganization);
 router.put('/resources/:id', updateOrganizationResources);
 router.delete('/:id', deleteOrganization);
+router.delete('/user/:id', deleteOrganizationByUserId);
 
 export default router;
